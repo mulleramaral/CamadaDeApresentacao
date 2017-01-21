c943b9088ca166f5bb9c5d60758d97d1deefee56
miiller caetano do amaral <miilleramaral@gmail.com>
2017-01-21 15:35:16 -0200

    Concluido Camada de apresentação

---
+++ CamadaDeApresentacao/Controllers/PostController.cs
@@ -0,0 +1,30 @@
+﻿using CamadaDeApresentacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CamadaDeApresentacao.Controllers
+{
+    public class PostController : Controller
+    {
+        public ActionResult Lista()
+        {
+            ICollection<Post> posts = new List<Post>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                posts.Add(new Post
+                {
+                    Autor = "Autor do post" + i,
+                    Categoria = "Categoria " + i,
+                    Data = DateTime.Now.Subtract(TimeSpan.FromDays(i)),
+                    Texto = i + " - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    Titulo = "Titulo do post " + i
+                });
+            }
+            return View(posts);
+        }
+    }
+}
---
+++ CamadaDeApresentacao/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CamadaDeApresentacao.Models
+{
+    public class Post
+    {
+        public string Autor { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Categoria { get; set; }
+
+        public string Texto { get; set; }
+    }
+}
